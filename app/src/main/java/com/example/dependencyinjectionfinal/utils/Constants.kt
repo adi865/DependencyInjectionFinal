dae027215dc139636cf54d83074c4cfbe0ec7eb2
tenxmacbookuser <adeel_ahmed_19@hotmail.com>
2023-04-04 11:37:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/dependencyinjectionfinal/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.dependencyinjectionfinal.utils
+
+object Constants {
+    const val API_KEY = "***"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+}
